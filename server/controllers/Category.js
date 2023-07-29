3e5559bedf5bb422409380df1f49f3d9521f6d5d
sarthak <sarthaksri044@gmail.com>
2023-07-30 01:05:35 +0530

    categoryPageDetails

--- server/controllers/Category.js
+++ server/controllers/Category.js
@@ -15,7 +15,7 @@ exports.createCategory = async (req, res) => {
 		console.log(CategorysDetails);
 		return res.status(200).json({
 			success: true,
-			message: "Categorys Created Successfully",
+			message: "Category Created Successfully",
 		});
 	} catch (error) {
 		return res.status(500).json({
@@ -42,3 +42,42 @@ exports.showAllCategories = async (req, res) => {
 		});
 	}
 };
+exports.categoryPageDetails = async (req, res) => {
+    try {
+           
+            const {categoryId} = req.body;
+            //get courses for specified categoryId
+            const selectedCategory = await Category.findById(categoryId)
+                                            .populate("courses")
+                                            .exec();
+           
+            if(!selectedCategory) {
+                return res.status(404).json({
+                    success:false,
+                    message:'Data Not Found',
+                });
+            }
+            //get coursesfor different categories
+            const differentCategories = await Category.find({
+                                         _id: {$ne: categoryId},
+                                         })    //not equal
+                                         .populate("courses")
+                                         .exec();
+
+            return res.status(200).json({
+                success:true,
+                data: {
+                    selectedCategory,
+                    differentCategories,
+                },
+            });
+
+    }
+    catch(error ) {
+        console.log(error);
+        return res.status(500).json({
+            success:false,
+            message:error.message,
+        });
+    }
+}
