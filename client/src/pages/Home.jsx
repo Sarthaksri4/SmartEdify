57a32c428e7a99b4fca1d2e7c3b6108e8d122f14
sarthak <sarthaksri044@gmail.com>
2024-02-08 23:41:11 +0530

    Home Page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import {FaArrowRight} from "react-icons/fa"
 import {Link} from "react-router-dom"
+import HighlightText from '../components/core/HomePage/HighlightText'
+
+import CTAButton from "../components/core/HomePage/Button"
+import Banner from "../assets/Images/banner.mp4"
 
 const Home = () => {
   return (
@@ -19,8 +23,37 @@ const Home = () => {
           </div>
 
       </Link>
-    </div>
+
+      <div className='text-center text-4xl font-semibold mt-7'>
+            Empower Your Future with
+            <HighlightText text={"Coding Skills"} />
+        </div>
+
+        <div className=' mt-4 w-[90%] text-center text-lg font-bold text-richblack-300'>
+            With our online coding courses, you can learn at your own pace, from anywhere in the world. 
+        </div>
+
+        <div className='flex flex-row gap-7 mt-8'>
+            <CTAButton active={true} linkto={"/signup"}> 
+                Learn More
+            </CTAButton>
+
+            <CTAButton active={false} linkto={"/login"}> 
+                Book a Demo
+            </CTAButton>
+        </div>
+
+        <div className='mx-3 my-12 shadow-blue-200'>
+            <video
+            muted
+            loop
+            autoPlay
+            >
+            <source  src={Banner} type="video/mp4" />
+            </video>
+        </div>
 </div>
+    </div>
 
   )
 }
